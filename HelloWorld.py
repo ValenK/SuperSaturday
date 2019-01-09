67ecaaee6f13a0a1573cc163e386c74cd745c9e6
ValenK <46509766+ValenK@users.noreply.github.com>
2019-01-08 21:22:13 -0500

    Create HelloWorld.py

---
+++ HelloWorld.py
@@ -0,0 +1,34 @@
+def valenf(V):
+	d=V+V
+	return d
+	#print(d)
+
+def xsq(x):
+	return x*x
+print(xsq(2))
+
+
+#i=0
+
+#T="Hello World"
+
+
+#print(T[0])
+
+
+#y=[]
+#L=[0,1,2,3,4,5]
+#for x in L:
+	#print(x)
+	#y.append(x)
+	#i+=1
+	#print(i)
+	
+#print(y)
+
+
+
+
+
+result=valenf(8)	
+print(result)
